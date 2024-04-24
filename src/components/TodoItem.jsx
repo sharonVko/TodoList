715383c85ed97e980035a810817a1446c00d65c5
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-04-24 12:14:45 +0200

    better todo-item

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 const TodoItem = () => {
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
   return (
     <section className="todo-item">
       <input
         className="todo-input"
         type="text"
         placeholder="add a todo"
-        value=""
+        value={name}
+        onChange={handleChange}
       />
       <button className="delete-btn">delete</button>
       <button className="check-btn" type="checkbox">
