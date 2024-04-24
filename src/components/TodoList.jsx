e14682175e33be4b3c0869e0319d7f5e0ac93a3a
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-04-24 11:32:26 +0200

    sth

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,8 @@
 const TodoList = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 };
 export default TodoList;
