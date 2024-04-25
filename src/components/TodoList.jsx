595ea30135952509b4701b3ff9636af9d7f650f7
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-04-25 15:25:40 +0200

    added some functionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,29 @@
 import TodoItem from "./TodoItem";
+import { useState } from "react";
+
+/*[{ id: 1 }, { id: 2 }, { id: 3 }];*/
+//mit useState Anzeige todos aktualisieren
 const TodoList = () => {
+  const [todos, setTodos] = useState([]);
   return (
     <>
-      <h1 className="text-3xl font-bold underline">fancy todo list</h1>
-      <TodoItem />
+      <h1 className="text-3xl font-bold">fancy todo list</h1>
+      <button
+        onClick={() => {
+          const newTodo = {
+            id: todos.length + 1,
+            value: "test",
+            checked: false,
+          };
+          setTodos([...todos, newTodo]);
+        }}
+      >
+        click
+      </button>
+
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} />
+      ))}
     </>
   );
 };
